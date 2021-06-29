36c6cedf5c606736fd6f4399e5a8f0c37504dd4b
vanct <vanct10a11999@gmail.com>
2021-06-29 14:39:33 +0700

    Bai 8

---
+++ sever-Time/src/main/java/SeverTimeServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "SeverTimeServlet", urlPatterns = "/index")
+public class SeverTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
